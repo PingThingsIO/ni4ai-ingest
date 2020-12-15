0e124ecae115d234c5ab1574c22d3ddb0131b763
laurelndunn <laurelndunn@gmail.com>
2020-12-14 16:29:07 -0800

    adding ingestors developed to date

---
+++ nomenclature.py
@@ -0,0 +1,28 @@
+nomenclature = {'Frequency': ('frequency', 'Hz'), 
+                
+                'Positive Seq Volt Mag': ('pos_seq_volt_mag', 'volts'),
+                'Positive Seq I Mag': ('pos_seq_current_mag', 'amps'),
+                'Positive Seq V Ang': ('pos_seq_volt_angle', 'deg'),
+                'Positive Seq I Ang': ('pos_seq_current_angle', 'deg'),
+                
+                'Phase A Volt Mag': ('phase_a_volt_mag', 'volts'),
+                'Phase B Volt Mag': ('phase_b_volt_mag', 'volts'),
+                'Phase C Volt Mag': ('phase_c_volt_mag', 'volts'),
+                
+                'Phase A Volt Ang': ('phase_a_volt_ang', 'deg'),
+                'Phase B Volt Ang': ('phase_b_volt_ang', 'deg'),
+                'Phase C Volt Ang': ('phase_c_volt_ang', 'deg'),
+                
+                'Phase A I Mag': ('phase_a_current_mag', 'amps'),
+                'Phase B I Mag': ('phase_b_current_mag', 'amps'),
+                'Phase C I Mag': ('phase_c_current_mag', 'amps'),
+                
+                'Phase A I Ang': ('phase_a_current_ang', 'deg'),
+                'Phase B I Ang': ('phase_b_current_ang', 'deg'),
+                'Phase C I Ang': ('phase_c_current_ang', 'deg'),
+                
+                'P (in MW)': ('real_power', 'MW'),
+                'Q (in MVAR)': ('reactive_power', 'MVAR')
+
+               }
+                
